c6213cc54c26c7945b23d227379097d07e5e6859
Oswaldo Montalvo <jomontalvo@hotmail.com>
2025-06-10 09:57:18 -0600

    Enable Scalar and ConnectionStrings in appsettings.json

--- SolCnbs.Api/Program.cs
+++ SolCnbs.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 using SolCnbs.Api.Endpoints;
 using SolCnbs.Data.Context;
 using SolCnbs.Data.Repositories;
@@ -22,20 +23,15 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-//if (app.Environment.IsDevelopment())
-//{
 app.MapOpenApi();
-app.UseSwaggerUI(c =>
-{
-c.SwaggerEndpoint("../openapi/v1.json", "TGR-1 SOL Api");
-c.RoutePrefix = "swagger";
-});
-//}
+
+app.MapScalarApiReference();
+
 app.UseHttpsRedirection();
 app.UseCors();
 
 #region Endpoints Configuration
-app.MapGroup("/api/moldes/configuracion").MapTemplatesConfiguration();
+app.MapGroup("/api/config").MapTemplatesConfiguration();
 app.MapGroup("/api/auditores").MapAuditorsRegistry();
 #endregion Endpoints Configuration
 
